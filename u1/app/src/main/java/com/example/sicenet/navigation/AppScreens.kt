68063b521c2d41f2c02ebb0466182faafdc5e3e8
QuintinoLgg <s20120151@alumnos.itsur.edu.mx>
2024-02-18 23:26:47 -0600

    Eliminacion de una carpeta erronea

--- u1/app/src/main/java/com/example/sicenet/navigation/AppScreens.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sicenet.navigation
-
-sealed class AppScreens(val route: String){
-    object HomeScreen: AppScreens("home_screen")
-    object LoginScreen: AppScreens("login_screen")
-}
